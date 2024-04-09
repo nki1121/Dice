27ccbae4e9ee12cf52e5bea60753451d8a67ee03
nki1121 <nkiuniv@gmail.com>
2024-04-09 22:33:20 +0900

    画像の装飾

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -4,6 +4,10 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "die.face.1")
+                .resizable() // 画像の大きさ変更時につける
+                .scaledToFit() // 画像の縦横の比率を固定する
+                .frame(width: UIScreen.main.bounds.width/2) //オブジェクトの大きさを変更する.画面の横幅をの半分の大きさを取得する
+                .padding() // 内側に余白をつける
             Button {
                 print("ボタンが押されたよ")
             } label: {
