9e67b2f989b63aa75d4cfe95219b592035f5a34e
nki1121 <nkiuniv@gmail.com>
2024-04-09 22:38:40 +0900

    間隔を変更する

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -3,16 +3,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "die.face.1")
                 .resizable() // 画像の大きさ変更時につける
                 .scaledToFit() // 画像の縦横の比率を固定する
                 .frame(width: UIScreen.main.bounds.width/2) //オブジェクトの大きさを変更する.画面の横幅をの半分の大きさを取得する
                 .padding() // 内側に余白をつける
+            Spacer()
             Button {
                 print("ボタンが押されたよ")
             } label: {
                 Text("サイコロを振る")
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.black) // 文字色を黒にする
+                    .cornerRadius(10) // ボタンの角に丸みをつける
             }
+            Spacer()
         }
         .padding()
     }
