af87547f41ef937c26f1d273988c70c655e4a774
nki1121 <nkiuniv@gmail.com>
2024-04-09 22:15:36 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by 中井晋平 on 2024/04/09.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
